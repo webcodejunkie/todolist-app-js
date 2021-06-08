a1e91746d2c5492bd350df27a3be6e915c3d63b3
webcodejunkie <webcodejunkie@gmail.com>
2021-06-07 23:47:36 -0400

    included comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 function newTodoItem(){
   // get the value of the input
   let addTodoValue = $('input').val();
-
+// conditional for checking if input isnt filled out
   if (addTodoValue === '') {
     alert('Enter a todo item!');
   } else {
-
+// else, add a todo to the list
     let li = $('<li></li>');
     li.addClass('todo-items');
-
+// delete button included
     let deleteElement = $('<span class="delete_element">Delete</span>');
     deleteElement.on('click', function() {
       li.remove();
     });
-
+// complete button to complete todos
     let completedElement = $('<span class="complete_element">Complete</span>');
     completedElement.on('click', function() {
       li.appendTo('.completed-list');
@@ -22,7 +22,7 @@ function newTodoItem(){
 
 
     });
-
+// append elements together
     li.append(addTodoValue);
     li.append(deleteElement);
     li.append(completedElement);
